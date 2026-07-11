c97a8e3bb886ebf9178160f8462f1622de97de33
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-11 21:10:14 +0530

    if-else program

---
+++ if-else.java
@@ -0,0 +1,20 @@
+public class if-else {
+    public static void main(String[] args) {
+        String day = "Saturday";
+        if(day=="Monday"){
+            System.out.println("It is weekday");
+        }else if(day=="Tuesday"){
+            System.out.println("It is weekday");
+        }else if(day=="Wednesday"){
+            System.out.println("It is weekday");
+        }else if(day=="Thursday"){
+            System.out.println("It is weekday");
+        }else if(day=="Friday"){
+            System.out.println("It is weekday");
+        }else if(day=="Saturday"){
+            System.out.println("It is weekend");
+        }else if(day=="Sunday"){
+            System.out.println("It is weekend");
+        } 
+    }
+}

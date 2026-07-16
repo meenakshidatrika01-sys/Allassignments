f1f2abf6a95762f1057d813e7a4bc1b39cf63a33
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-16 22:22:01 +0530

    variablee2

--- Variablee2.java
+++ Variablee2.java
@@ -1,3 +1,11 @@
 public class Variablee2 {
-    public static void main
+    public static void main(String[] args) {
+        int number = 6;
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
 }

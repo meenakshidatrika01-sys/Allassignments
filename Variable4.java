13265e47bcb9c807eb8abfb3743f6599ee7e9a89
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-16 22:43:14 +0530

    Variable4

---
+++ Variable4.java
@@ -0,0 +1,12 @@
+public class Variable4 {
+    public static void main(String[] args) {
+        int marks = 20;
+
+        if (marks >= 35) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
+

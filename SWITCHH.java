51285c0ad9efb30866aba06376e659648100c99e
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-16 22:03:30 +0530

    SWITCHH

---
+++ SWITCHH.java
@@ -0,0 +1,31 @@
+public class SWITCHH{
+    public static void main(String[] args){
+        int day = 5;
+        switch(day){
+            case 1: 
+                System.out.println("It is weekday");
+                break;
+            case 2: 
+                System.out.println("It is weekday");
+                break;
+            case 3: 
+                System.out.println("It is weekday");
+                break;
+            case 4: 
+                System.out.println("It is weekday");
+                break;
+            case 5: 
+                System.out.println("It is weekday");
+                break;
+            case 6: 
+                System.out.println("It is weekend");
+                break;
+            case 7: 
+                System.out.println("It is weekend");
+                break;
+            default :
+                System.out.println("Invaild day, please enter a vaild day");
+                break;
+        }
+    }
+}
---
+++ Variablee1.java
@@ -0,0 +1,10 @@
+public class Variablee1{
+    public static void main(String[] args){
+        int number = -3;
+        if(number >= 0) {
+            System.out.println("positive number");
+        }else {
+            System.out.println("negative number");
+        } 
+    }
+}

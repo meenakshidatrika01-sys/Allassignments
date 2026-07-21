0838a89aa8e186331e00fd585c7d27c06590a562
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-21 22:03:38 +0530

    Arraystring1

---
+++ Arraystring1.java
@@ -0,0 +1,8 @@
+public class Arraystring1 {
+    public static void main(String[] args) {
+        String[] colours= {"black","red","blue","pink"};
+        for(int i=0; i<colours.length; i++){
+            System.out.println(colours[i]);
+        }
+    }
+}

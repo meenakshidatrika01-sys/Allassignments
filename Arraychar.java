e6a339ce017857abbc1f63de59c1c2abf775b503
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-21 21:54:35 +0530

    Array

---
+++ Arraychar.java
@@ -0,0 +1,8 @@
+public class Arraychar {
+    public static void main(String[] args) {
+        char[] letters = {'H','e','l','l','o'};
+        for(int i=0; i<letters.length; i++){
+            System.out.println(letters[i]);
+        }
+    }
+}
---
+++ Arrayy.java
@@ -0,0 +1,11 @@
+public class Arrayy {
+    public static void main(String[] args) {
+        int[] ages = new int[5];
+        ages[0]=18;
+        ages[1]=20;
+        ages[2]=22;
+        ages[3]=24;
+        ages[4]=26;
+        System.out.println("ages[2]");
+    }
+}

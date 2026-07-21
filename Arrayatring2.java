8679a2bc23f2137c7c04227a9679bb245f18b316
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-21 22:12:01 +0530

    Arrayatring2

---
+++ Arrayatring2.java
@@ -0,0 +1,8 @@
+public class Arrayatring2 {
+    public static void main(String[] args) {
+        String[] colours= {"black","red","blue","pink"};
+        for(String colour : colours) {
+            System.out.println(colour);
+        }
+    }
+}

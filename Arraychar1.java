a7a2ff057da26d17b3405cbf20e30bae23dafeb8
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-21 21:58:32 +0530

    Arraychar1

--- Arraychar1.java
+++ Arraychar1.java
@@ -1,4 +1,4 @@
-public class Arraychar {
+public class Arraychar1 {
     public static void main(String[] args) {
         char[] letters = {'H','e','l','l','o'};
         for(int i=0; i<letters.length; i++){

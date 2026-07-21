544044ad74d762a6575b1d746e6aa4b609ed1ec1
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-21 22:07:40 +0530

    Arraychar2

---
+++ Arraychar2.java
@@ -0,0 +1,8 @@
+public class Arraychar2 {
+    public static void main(String[] args){
+        char[] letters = {'H','e','l','l','o'};
+        for(char ch : letters){
+            System.out.println(ch);
+        }    
+    }
+}

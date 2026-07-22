b4ae95c1e6add0b6ec9bf68c6976e1f693b0ca55
meenakshidatrika01-sys <meenakshidatrika01@gmail.com>
2026-07-22 22:39:31 +0530

    getters and setters

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("meenakshi");
+        student.setAge(20);
+        System.out.println("Name: " + student.getName());
+        System.out.println("Age: " + student.getAge());
+    }
+    
+}
